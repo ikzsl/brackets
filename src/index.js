ebfa0fcf362fc461adcb43a8b12a783dc149cb06
Муслим Джалалудинов <ikzsl@yandex.ru>
2022-01-25 23:34:23 +0300

    add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairsSubsrts = bracketsConfig.map(([left, right]) => left + right);
+  let tempString = str;
+  while (pairsSubsrts.some((pair) => tempString.includes(pair))) {
+    pairsSubsrts.forEach((substr) => {
+      tempString = tempString.replaceAll(substr, "")
+    })
+  }
+  return !tempString.length;
 }
